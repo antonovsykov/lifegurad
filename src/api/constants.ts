4309e67514202cf83e72ef6ee84baa57ec6c0d8b
PC-20241021WOVA\Administrator <240892907@qq.com>
2025-08-31 09:05:29 +0800

    update https test

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,2 +1,2 @@
-export const WEBUI_BASE_URL = `http://test.lifeguard.chat`;
+export const WEBUI_BASE_URL = `https://test.lifeguard.chat`;
 // export const WEBUI_BASE_URL = `http://${location.hostname}:8080`;
